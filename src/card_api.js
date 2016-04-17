4b3d755a84b2d3672e660e6a576adadd7552161e
Jaebum Lee <kev0960@gmail.com>
2016-04-17 08:38:22 +0000

    Forget to push card-api.js

---
+++ src/card_api.js
@@ -0,0 +1,61 @@
+const unirest = require('unirest');
+
+function HearthAPI() {
+  this.card_db = [];
+  this.get_card_db();
+}
+HearthAPI.prototype.get_card_db = function() {
+  unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards").header("X-Mashape-Key", "nPCuh0xMwxmshf9ZGfRS2p3lF7Jip1pJbjYjsn67kOlM4TTr7j").end(function(result) {
+    var data = result.body;
+    var collection;
+
+    // JSON File Format
+    // { name, cardSet, type, faction (호드인지 얼라인지, 중립인지), rarity, race, cost, attack, health, playerClass, img }
+    // rarity : Free, Common, Rare, Epic, Legendary
+    for (collection in data) {
+      console.log(collection, data[collection].length);
+
+      if (collection == 'Debug' || collection == 'Tavern Brawl' || collection == 'Missions') continue;
+      for (var i = 0; i < data[collection].length; i++) {
+        if (!data[collection][i].name) continue;
+
+        var t = data[collection][i].type;
+        if (t == 'Minion' || t == 'Weapon' || t == 'Spell') {
+          var c = data[collection][i];
+          hearth_api.card_db.push({
+              name : c.name.replace('\\', ''),
+              type: c.type.toLowerCase(),
+              level: (c.rarity ? c.rarity.toLowerCase() : null),
+              kind: (c.race ? c.race.toLowerCase() : null),
+              job: (c.playerClass ? c.playerClass.toLowerCase() : null),
+              img: c.img,
+              info: [c.cost, (c.attack ? c.attack : 0), (c.health ? c.health : 0)],
+              unique : c.cardId,
+              is_token : (c.collectible ? false : true)
+          });
+        }
+      }
+    }
+    console.log('Done downloading ..');
+  });
+}
+
+var hearth_api = new HearthAPI();
+
+module.exports = {
+  get_db: hearth_api.get_card_db,
+  load_card: function(name) {
+    var card = null;
+    for (var i = 0; i < hearth_api.card_db.length; i++) {
+      if (hearth_api.card_db[i].name == name) {
+        card = hearth_api.card_db[i];
+        break;
+      }
+    }
+    if(card) {
+      
+    }
+    console.log(card, " with ", name, ' db size : ', hearth_api.card_db.length);
+    return [name, card.type, card.level, card.job, card.info[0], card.info[1], card.info[2], card.kind];
+  }
+};
